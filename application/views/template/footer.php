509fb46bb97ab1244a5d3b021dfa6c5942ae72f1
bernandotorrez <bernandotorrez4@gmail.com>
2018-11-04 13:50:09 +0700

    update template

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,11 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
- <footer class="footer text-info">
-        <div class="container text-info">
-           
-            <div class="copyright pull-right text-center">
-              <font class="text-info"> &copy; <script>document.write(new Date().getFullYear())</script>, Bernand D H @ Universitas Pembangunan Nasional "Veteran" Jakarta </font>
-            </div>
-        </div>
-    </footer>
+
+ <footer>
+     <!-- place your html footer here   -->
+ </footer>
--- application/views/template/template.php
+++ application/views/template/template.php
@@ -1,260 +1,37 @@
-
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
+  <title><?=WEB_TITLE;?></title>
+
+  <!-- Meta tag -->
   <meta charset="utf-8" />
-  
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
-  <title><?=$title;?></title>
-  <link rel="icon" href="<?=base_url('template/assets/img/favicon/favicon.ico');?>" type="image/x-icon" />
-<link rel="shortcut icon" href="<?=base_url('template/assets/img/favicon/favicon.ico');?>" type="image/x-icon" />
-  <link rel="apple-touch-icon" sizes="57x57" href="<?=base_url('template/assets/img/favicon/apple-icon-57x57.png');?>">
-<link rel="apple-touch-icon" sizes="60x60" href="<?=base_url('template/assets/img/favicon/apple-icon-60x60.png');?>">
-<link rel="apple-touch-icon" sizes="72x72" href="<?=base_url('template/assets/img/favicon/apple-icon-72x72.png');?>">
-<link rel="apple-touch-icon" sizes="76x76" href="<?=base_url('template/assets/img/favicon/apple-icon-76x76.png');?>">
-<link rel="apple-touch-icon" sizes="114x114" href="<?=base_url('template/assets/img/favicon/apple-icon-114x114.png');?>">
-<link rel="apple-touch-icon" sizes="120x120" href="<?=base_url('template/assets/img/favicon/apple-icon-120x120.png');?>">
-<link rel="apple-touch-icon" sizes="144x144" href="<?=base_url('template/assets/img/favicon/apple-icon-144x144.png');?>">
-<link rel="apple-touch-icon" sizes="152x152" href="<?=base_url('template/assets/img/favicon/apple-icon-152x152.png');?>">
-<link rel="apple-touch-icon" sizes="180x180" href="<?=base_url('template/assets/img/favicon/apple-icon-180x180.png');?>">
-<link rel="icon" type="image/png" sizes="192x192"  href="<?=base_url('template/assets/img/favicon/android-icon-192x192.png');?>">
-<link rel="icon" type="image/png" sizes="32x32" href="<?=base_url('template/assets/img/favicon/favicon-32x32.png');?>">
-<link rel="icon" type="image/png" sizes="96x96" href="<?=base_url('template/assets/img/favicon/favicon-96x96.png');?>">
-<link rel="icon" type="image/png" sizes="16x16" href="<?=base_url('template/assets/img/favicon/favicon-16x16.png');?>">
-<link rel="manifest" href="<?=base_url('template/assets/img/favicon/manifest.json');?>">
-  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-  
-  <meta name="keywords" content="Jujitsu, Indonesia Jujitsu, Jujitsu Dojo, Jujitsu Dojo Pondok Labu, Dojo Jujitsu, Universitas Pembangunan Nasional Veteran Jakarta, UPN Veteran Jakarta, Jujitsu Universitas Pembangunan Nasional Veteran Jakarta, Jujitsu UPN Veteran Jakarta, UKM Jujitsu, Website Jujitsu UPN Veteran Jakarta">
-    <meta name="description" content="Website Pendaftaran Online Anggota Jujitsu UPN Veteran Jakarta serta Website Portal kegiatan - kegiatan UKM Jujitsu UPN Veteran Jakarta">
-
-   />
+  <meta name="keywords" content="your web keyword">
+  <meta name="description" content="your web description">
   <meta name="author" content="Bernand D H">
-    <!-- Schema.org markup for Google+ -->
-    <meta itemprop="name" content="Bernand D H">
-    <meta itemprop="description" content="Website Pendaftaran Online Anggota Jujitsu UPN Veteran Jakarta serta Website Portal kegiatan - kegiatan UKM Jujitsu UPN Veteran Jakarta">
-    <meta itemprop="image" content="https://pbs.twimg.com/profile_images/378800000433502142/638c93134ebf1e2fe50cc0d9ce6a825e.jpeg">
+  <!-- Meta tag -->
 
-    <!-- Twitter Card data -->
-    <meta name="twitter:card" content="Bela Diri">
-    <meta name="twitter:site" content="Website Jujitsu UPN Veteran Jakarta">
-    <meta name="twitter:title" content="Website Jujitsu UPN Veteran Jakarta">
-    <meta name="twitter:description" content="Website Pendaftaran Online Anggota Jujitsu UPN Veteran Jakarta serta Website Portal kegiatan - kegiatan UKM Jujitsu UPN Veteran Jakarta">
-    <meta name="twitter:creator" content="Bernand D H">
-    <meta name="twitter:image" content="https://pbs.twimg.com/profile_images/378800000433502142/638c93134ebf1e2fe50cc0d9ce6a825e.jpeg">
-    <meta name="twitter:data1" content="Website Jujitsu UPN Veteran Jakarta">
-    <meta name="twitter:label1" content="Website Jujitsu UPN Veteran Jakarta">
-    <meta name="twitter:data2" content="Website Jujitsu UPN Veteran Jakarta">
-    <meta name="twitter:label2" content="Website Jujitsu UPN Veteran Jakarta">
-
-    <!-- Open Graph data -->
-    <meta property="og:title" content="Website Jujitsu UPN Veteran Jakarta">
-    <meta property="og:type" content="article">
-    <meta property="og:url" content="https://jujitsu-upn.online">
-    <meta property="og:image" content="https://pbs.twimg.com/profile_images/378800000433502142/638c93134ebf1e2fe50cc0d9ce6a825e.jpeg">
-    <meta property="og:description" content="Website Pendaftaran Online Anggota Jujitsu UPN Veteran Jakarta serta Website Portal kegiatan - kegiatan UKM Jujitsu UPN Veteran Jakarta">
-    <meta property="og:site_name" content="Website Jujitsu UPN Veteran Jakarta">
-
-
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="<?=base_url('template/assets/img/favicon/ms-icon-144x144.png');?>">
-<meta name="theme-color" content="#ffffff">
-
-  <!-- Canonical SEO -->
-  <link rel="canonical" href="http://www.creative-tim.com/product/material-kit-pro" />
-
-  <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
-
-  <!-- CSS Files -->
-    <link href="<?=base_url('template/assets/css/bootstrap.min.css');?>" rel="stylesheet" />
-    <link href="<?=base_url('template/assets/css/material-kit.css');?>?v=1.2.1" rel="stylesheet"/>
-
-  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <!-- Place your css here -->
   <link href="<?=base_url('template/assets/css/vertical-nav.css');?>" rel="stylesheet" />
-  <link href="<?=base_url('template/assets/css/toastr.css');?>" rel="stylesheet" />
-  <link href="<?=base_url('template/assets/css/demo.css');?>" rel="stylesheet" />
+  <!-- Place your css here -->
 </head>
+<body>
 
-<body class="<?=$class;?>">
-<?php echo $_menu;?>
-    
-
-
-             <div id="error1">
-        <!-- error will be shown here ! -->
-        </div>
-    
-    
+    <!-- Tampilkan menu -->
+    <?php echo $_menu;?>
+    <!-- Tampilkan menu -->
+  
+    <!-- Tampilkan content web -->
     <?php echo $_content;?>
+    <!-- Tampilkan menu -->
     
-    
+    <!-- Tampilkan footer -->
+    <?php echo $_footer; ?>
+    <!-- Tampilkan footer -->
 
-    <?php 
-    if($url=='Home' OR $url=='home' OR $url=='Home/index' OR $url=='home/index'){
-        
-    } else {
-    
-    echo $_footer;
-    
-    }
-    ?>
-    
- </body>         
- 
+    <!-- Place your js here -->
+    <script src="<?=base_url('template/assets/js/bootstrap.min.js');?>" type="text/javascript"></script>
+    <!-- Place your js here -->
 
-  
-
-  <!--     *********    END CONTACT US 2      *********      -->
-
-  
-  <?php
-  if($url=='Home/index' OR $url=='Home') {
-    ?>
-    <nav id="cd-vertical-nav">
-        <ul>
-            <li>
-                <a href="#home" data-number="1">
-                    <span class="cd-dot"></span>
-                    <span class="cd-label">Home</span>
-                </a>
-            </li>
-            <li>
-                <a href="#sejarah" data-number="2">
-                    <span class="cd-dot"></span>
-                    <span class="cd-label">Sejarah</span>
-                </a>
-            </li>
-            <li>
-                <a href="#informasi" data-number="3">
-                    <span class="cd-dot"></span>
-                    <span class="cd-label">Informasi</span>
-                </a>
-            </li>
-             <li>
-                <a href="#anggota" data-number="4">
-                    <span class="cd-dot"></span>
-                    <span class="cd-label">Anggota</span>
-                </a>
-            </li>
-            <li>
-                <a href="#pelatih" data-number="5">
-                    <span class="cd-dot"></span>
-                    <span class="cd-label">Pelatih</span>
-                </a>
-            </li>
-            <li>
-                <a href="#kesan" data-number="6">
-                    <span class="cd-dot"></span>
-                    <span class="cd-label">Kesan</span>
-                </a>
-            </li>
-            
-            <li>
-                <a href="#lokasi" data-number="7">
-                    <span class="cd-dot"></span>
-                    <span class="cd-label">Lokasi</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
-
-<?php } ?>
-
-  <!--   Core JS Files   -->
- <script
-  src="https://code.jquery.com/jquery-3.2.1.min.js"
-  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-  crossorigin="anonymous"></script>
-  <script src="<?=base_url('template/assets/js/bootstrap.min.js');?>" type="text/javascript"></script>
-  <script src="<?=base_url('template/assets/js/material.min.js');?>"></script>
-
-  <!--    Plugin for Date Time Picker and Full Calendar Plugin   -->
-  <script src="<?=base_url('template/assets/js/moment.min.js');?>"></script>
- 
-  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/   -->
-  <script src="<?=base_url('template/assets/js/nouislider.min.js');?>" type="text/javascript"></script>
-
-  <!--  Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker   -->
-  <script src="<?=base_url('template/assets/js/bootstrap-datetimepicker.js');?>" type="text/javascript"></script>
-
-  <!--  Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select   -->
-  <script src="<?=base_url('template/assets/js/bootstrap-selectpicker.js');?>" type="text/javascript"></script>
-
-  <!--  Plugin for Tags, full documentation here: http://xoxco.com/projects/code/tagsinput/   -->
-  <script src="<?=base_url('template/assets/js/bootstrap-tagsinput.js');?>"></script>
-
-  <!--  Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput   -->
-  <script src="<?=base_url('template/assets/js/jasny-bootstrap.min.js');?>"></script>
-<script src="<?=base_url('template/assets/js/toastr.min.js');?>"></script>
-<script src="<?=base_url('template/assets/js/modernizr.js');?>"></script>
-<script src="<?=base_url('template/assets/js/vertical-nav.js');?>"></script>
-  <!--    Plugin For Google Maps   -->
-
-  <!--    Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc    -->
- 
-  <script src="<?=base_url('template/assets/js/material-kit.js');?>?v=1.2.1" type="text/javascript"></script>
-<?php
-if($url=='home/kontak' OR $url=='Home/kontak' OR $url=='Home' OR $url=='home' OR $url=='Home/index' OR $url=='home/index' OR $url=='') {
-?>
-<script  type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAtk1gh4x3chHpX2LpYOI4dYDmeImnMXBY"></script>
-  <script type="text/javascript">
-    $().ready(function(){
-
-      
-      materialKitDemo.initContactUs2Map();
-    });
-  </script>
-<?php } ?>
- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
-
-   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
-<?php
-if($url=='register' OR $url=='register/index') {
-?>
-<script src="<?=base_url('template/assets/js/register.js');?>" type="text/javascript"></script>
-<?php } ?>
-
-<?php
-if($url=='login' OR $url=='login/index') {
-?>
-<script src="<?=base_url('template/assets/js/login.js');?>" type="text/javascript"></script>
-<?php } ?>
-
-<?php
-if($url=='reset' OR $url=='reset/index') {
-?>
-<script src="<?=base_url('template/assets/js/reset.js');?>" type="text/javascript"></script>
-<?php } ?>
-
-<?php
-if($url=='home/kontak' OR $url=='Home/kontak' OR $url=='Home' OR $url=='home' OR $url=='Home/index' OR $url=='home/index') {
-?>
-<script src="<?=base_url('template/assets/js/kontak.js');?>" type="text/javascript"></script>
-<?php } ?>
-
-<?php
-if($url=='Reset/perbarui' OR $url=='reset/perbarui') {
-?>
-<script src="<?=base_url('template/assets/js/perbarui.js');?>" type="text/javascript"></script>
-<?php } ?>
-
-<?php
-if($url=='Home/saran' OR $url=='home/saran') {
-?>
-<script src="<?=base_url('template/assets/js/saran.js');?>" type="text/javascript"></script>
-<?php } ?>
-
-<?php
-if($url=='Home/pendaftaran' OR $url=='home/pendaftaran') {
-?>
-<script src="<?=base_url('template/assets/js/pendaftaran.js');?>" type="text/javascript"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-chained/1.0.1/jquery.chained.min.js" type="text/javascript"></script>
-<script type="text/javascript">
-            $("#jurusan").chained("#fakultas");
-           materialKit.initFormExtendedDatetimepickers();
-          
-        </script>
-<?php } ?>
+</body>
 </html>
